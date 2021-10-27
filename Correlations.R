b6e7c8a32b06b0d5dba530c5662544a80481dec7
Christoffer Vissing <58069372+christoffervi@users.noreply.github.com>
2021-10-27 22:13:58 +0200

    Update Correlations.R

--- Correlations.R
+++ Correlations.R
@@ -1,7 +1,8 @@
 #CORRELATIONS in TTNtv
-library(GGally)
-dat_df$bac
+library(GGally); library(tidyverse)
 
+
+#Z-transforming muscle-strength values
 dat_df_scaled <- dat_df %>% select(MD_L_elbow_flex, MD_L_elbow_ext, MD_L_hip_ext, MD_L_hip_flex, MD_L_ankle_dflex,MD_L_ankle_pflex, MD_L_knee_ext, MD_L_knee_flex,
                                    MD_R_elbow_flex, MD_R_elbow_ext, MD_R_hip_ext, MD_R_hip_flex, MD_R_ankle_dflex,MD_R_ankle_pflex, MD_R_knee_ext, MD_R_knee_flex) %>%
   scale() %>% as_tibble() %>% rowwise() %>% 
@@ -11,7 +12,7 @@ dat_df_scaled <- dat_df %>% select(MD_L_elbow_flex, MD_L_elbow_ext, MD_L_hip_ext
           knee_strength = mean(c(MD_L_knee_ext, MD_L_knee_flex,MD_R_knee_ext, MD_R_knee_flex),na.rm=T),
          hip_strength = mean(c(MD_L_hip_ext, MD_L_hip_flex,MD_R_hip_ext, MD_R_hip_flex),na.rm=T))
 
-
+#Relevant dataset
 p <- dat_df %>% mutate(strength = dat_df_scaled$strength) %>%  
   select(back_FF, thigh_FF, calf_FF, Age, BMI,LVEF_bp,blood_proBNP, strength, `ipaq-7d-mettotal`, Gruppe) %>% 
   mutate(Gruppe = factor(Gruppe, labels = c("TTNtv", "Healthy")),
@@ -21,26 +22,6 @@ p <- dat_df %>% mutate(strength = dat_df_scaled$strength) %>%
          Back = back_FF,
          Thigh = thigh_FF,
          Calf = calf_FF)
-
-library(cowplot)
-#############################################
-# INFORMATION PLOT KENDALL
-#############################################
-  ggpairs(p,
-          ggplot2::aes(color =Gruppe), 
-          columnLabels = c("Back", "Thigh", "Calf", "Age","BMI", "LVEF","pro-BNP","Strength", "METS", "Group"),
-          lower = list(continuous = "smooth",combo = wrap("facethist", binwidth = 1)),
-          upper = list(continuous = wrap("cor", size = 2.6, method = "kendall", digits =2, alpha =1), combo = "dot_no_facet"),
-          axisLabels = "none",
-          #proportions = c(2,2,2,1,1,1,1,1,1),
-          switch = "y"
-         )+
-    scale_fill_scico_d(palette = "batlow", alpha = 0.5)+
-    scale_color_scico_d(palette = "batlow", alpha = 0.8)+
-    theme(text = element_text(family = "serif", size = 10),
-          axis.text.x = element_text(angle = 45),
-          axis.text.y = element_text())
-ggsave("correlation_kendall.png", units = "cm", height = 24, width =24, dpi = 1200)
 
 #############################################
 # INFORMATION PLOT pearson
@@ -61,36 +42,10 @@ ggpairs(p,
         axis.text.y = element_text())
 ggsave("correlation_pearson.png", units = "cm", height = 24, width =24, dpi = 1200)
 
-#############################################
-# INFORMATION PLOT spearman
-#############################################
-ggpairs(p,
-        ggplot2::aes(color =Gruppe), 
-        columnLabels = c("Back", "Thigh", "Calf", "Age","BMI", "LVEF","pro-BNP","Strength", "METS", "Group"),
-        lower = list(continuous = "smooth",combo = wrap("facethist", binwidth = 1)),
-        upper = list(continuous = wrap("cor", size = 2.6, method = "spearman", digits =2, alpha =1), combo = "dot_no_facet"),
-        axisLabels = "none",
-        #proportions = c(2,2,2,1,1,1,1,1,1),
-        switch = "y"
-)+
-  scale_fill_scico_d(palette = "batlow", alpha = 0.5)+
-  scale_color_scico_d(palette = "batlow", alpha = 0.8)+
-  theme(text = element_text(family = "serif", size = 10),
-        axis.text.x = element_text(angle = 45),
-        axis.text.y = element_text())
-ggsave("correlation_spearman.png", units = "cm", height = 24, width =24, dpi = 1200)
 
-
-cor(dat_df_ttn$thigh_FF,dat_df_ttn$LVEF_bp, use = "pairwise.complete.obs", method = "spearman")
-
-
-
-
-
-
-
-library(tidyverse)
-
+##############################################
+# DATA WRANGLING
+##############################################
 p <- dat_df %>% mutate(strength = dat_df_scaled$strength, knee_strength = dat_df_scaled$knee_strength,
                        hip_strength = dat_df_scaled$hip_strength) %>%  
   select(back_FF, thigh_FF, calf_FF, Age, BMI,LVEF_bp, blood_proBNP, strength, `ipaq-7d-mettotal`, Gruppe) %>% 
@@ -106,15 +61,9 @@ p <- dat_df %>% mutate(strength = dat_df_scaled$strength, knee_strength = dat_df
 
 
 library(RColorBrewer);library(Redmonder); library(pals)
-p1 <- p %>%  filter(Gruppe=="TTNtv")
-ggcorr(p1, geom= "tile", nbreaks = 8, 
-       method = c("pairwise.complete.obs", "kendall"), 
-       label = T, label_round = 2, label_color = "black",
-       size=4)+
-  scale_fill_brewer(palette = "RdBu", direction = -1)
-   # scale_fill_scico(palette = "batlow", direction = -1, alpha = 0.85)
-  #scale_fill_scico(palette = "roma", begin = 0.75, end = 0)
-ggsave("correlation_Kendall_step.png", units = "cm", height = 20, width =20, dpi = 800)
+###################################################
+# More simple plot for publication
+####################################################
 
 ggcorr(p1, geom= "tile",
        method = c("pairwise.complete.obs", "pearson"), 
@@ -122,16 +71,3 @@ ggcorr(p1, geom= "tile",
        size=4, high = "#7E1900", low = "#1A3399", mid = "#EAEDE9")
   scale_fill_brewer(palette = "RdBu", direction = -1)
 ggsave("correlation_Pearson_step_3.png", units = "cm", height = 20, width =20, dpi = 800)
-
-ggcorr(p, geom= "blank", 
-       method = c("pairwise.complete.obs", "spearman"), 
-       label = T, hjust=0.75, label_round = 2, size =5)+
-  geom_point(size = 14, aes(color = coefficient >0, alpha= abs(coefficient) > 0.5), show.legend = F) +
-  scale_alpha_manual(values = c("TRUE" = 0.4, "FALSE" = 0)) +
-  scale_color_manual(values = c("blue", "red"))
-ggsave("correlation_4_cont.png", units = "cm", height = 20, width =20, dpi = 1200)
-label_round = 2, label_color = "black",
-       size=4)
- pals::ocean.balance(3)
-scico(2, palette = "batlow")
-
